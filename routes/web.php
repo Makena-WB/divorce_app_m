a84d896f93da93fe43a950dac3d23d5508e84206
LynnMuchiri <100213983+LynnMuchiri@users.noreply.github.com>
2023-07-14 01:02:57 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LawyerController;
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\GoogleController;
+
 
 
 /*
